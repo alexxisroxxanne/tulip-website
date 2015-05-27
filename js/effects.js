4d1e4b7fc86c5d8255638844be153967ced727b5
Alexxis Johnson <alexxis.r.p.johnson@gmail.com>
2015-05-27 16:33:37 -0700

    Add buttons with functionality to carousel

--- js/effects.js
+++ js/effects.js
@@ -2,3 +2,54 @@
 	slideToggle("slow");
 });
 */
+
+/*
+function pauseCarousel() {
+	$('#playButton').click(function() {
+		$('#image-carousel').carousel('cycle');
+	});
+	$('#pauseButton').click(function() {
+		$('#image-carousel').carousel('pause');
+	});
+};
+*/
+
+/*
+$('#playButton').click(function() {
+	$('#image-carousel').carousel('cycle');
+});
+
+$('#pauseButton').click(function() {
+	$('#image-carousel').carousel('pause');
+});
+*/
+
+
+/*
+$(function(){
+    $('#image-carousel.slide').carousel({
+        interval: 5000,
+        pause: "click"
+    });
+
+    $('input').focus(function(){
+       $("#image-carousel").carousel('pause');
+    }).blur(function() {
+       $("#image-carousel").carousel('cycle');
+    });
+});
+*/
+
+
+
+$(function () {
+    $('#image-carousel').carousel({
+        pause: "false"
+    });
+    $('#playButton').click(function () {
+        $('#image-carousel').carousel('cycle');
+    });
+    $('#pauseButton').click(function () {
+        $('#image-carousel').carousel('pause');
+    });
+});
